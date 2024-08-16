0d821bdc4990aa1eeac97883ad898f54567db790
unknown <shohayabhasan@yahoo.com>
2024-08-16 09:04:00 +0600

    PROJECT STARTED

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      "poppings": ["Poppins", "sans-serif"]
+    },
+    colors: {
+      "yellow":"#FBD062",
+      "black":"#111430",
+      "white":"rgb(250 250 249)"
+    },
+
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+    },
+  },
+  plugins: [],
+}
